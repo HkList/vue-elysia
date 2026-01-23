6a17e7ce20e0c2a7cfd01ead256b0f3ca82dddac
huankong233 <huank0ng@foxmail.com>
2026-01-24 01:40:51 +0800

    [*] 修复脚本异常输出

--- scripts/run-command.ts
+++ scripts/run-command.ts
@@ -6,9 +6,17 @@ const args = process.argv.slice(2)
 const script = args[0] ?? 'dev'
 const packages = (args[1] ?? 'backend').split(',')
 
-concurrently(
+const { result } = concurrently(
   packages.map((app) => ({
     name: app,
     command: `bun run --cwd apps/${app} ${script} ${args.slice(2).join(' ')}`,
   })),
+  {
+    killOthersOn: ['failure'],
+  },
+)
+
+result.then(
+  () => process.exit(0),
+  () => process.exit(1),
 )
